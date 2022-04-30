7e52ca6aa2713ef66e36e516538c5f7bbe018385
Mahmoud GUEFRI <mamouty4@hotmail.com>
2022-04-30 10:14:14 +0100

    Use OAuth 2.0 & Sign is with Google

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate'); // This will allow the use of the findOrCreate function.
+
 
 const app = express();
 
@@ -31,20 +34,57 @@ const {Schema} = mongoose;
 
 const userSchema = new Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose); // This will be used save users, salt and hash their passwords.
+userSchema.plugin(findOrCreate); 
 
 const User = mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// used to serialize the user for the session
+passport.serializeUser(function(user, done) {
+    done(null, user.id); 
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+// Setting up the google strategy.
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res) {
     res.render("home");
+});
+
+// Authenticating the user using passport with the google strategy.
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })); // The profile here includes the email and user ID on Google.
+
+//Redirecting the user to our website. This get request is made by Google.
+app.get("/auth/google/secrets", 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
 });
 
 app.get("/login", function(req, res) {
